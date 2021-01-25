d59cd667681005960ec0c47bdd377138fabfe70a
yeasin-hastechit <yeasin@hastechit.com>
2021-01-25 17:25:25 +0600

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const 
+    gulp = require("gulp"),
+    clean = require('gulp-clean'),
+    imagemin = require("gulp-imagemin"),
+    webp = require("imagemin-webp"),
+    extReplace = require("gulp-ext-replace");
+
+gulp.task("cleanDist", function(done) {
+    gulp.src("./dist/")
+        .pipe(clean());
+    done();
+});
+
+gulp.task("exportWebP", function(done) {
+    let src = ["src/**/*.png", "src/**/*.jpg"];
+    let dest = "./dist/";
+    gulp.src(src)
+        .pipe(imagemin([
+            webp({
+                quality: 75
+            })
+        ]))
+        .pipe(extReplace(".webp"))
+        .pipe(gulp.dest(dest));
+    done();
+});
